460055eab667fbcdb1931dba27d9a2dcebef6c2e
Sam C <samrocksc@gmail.com>
2020-04-06 16:00:57 +0200

    Compose functional

--- src/index.ts
+++ src/index.ts
@@ -23,32 +23,75 @@ interface InputInterface {
   lernaLocation?: string;
 }
 
+interface GetConfigOutputInterface {
+  /** the list of packages to check for in lerna.json **/
+  packages: string[];
+  /** The version control method of lerna **/
+  version: string;
+}
+
+interface GetPackageNameListOutputInterface {
+  /** The name of the package **/
+  name: string;
+  /** the location of the package **/
+  location: string;
+}
+
+interface GetPackagesToUpdateInterface {
+  packageNameList: GetPackageNameListOutputInterface[];
+  packageJsonMap: PackageJsonMapInterface[];
+}
+
 /**
- * Checks package.json recursively in a lerna project and copies
- * that file into its node_modules.
+ * Gets the config file for lerna
  *
- * @async
- * @param {@link InputInterface} input - an input object for settings
- * @return {Promise<void>} - this is a simple void function
+ * @function getConfig
+ * @param {InputInterface} input - Options that you can run dewey with
+ * @return {GetConfigOutputInterface}
  */
-async function main(input?: InputInterface): Promise<void> {
+const getConfig = (input?: InputInterface): GetConfigOutputInterface => {
   const lernaConfigLocation =
     (input && input?.lernaLocation) || `${process.cwd()}/lerna.json`;
-  const lernaConfig = JSON.parse(readFileSync(lernaConfigLocation).toString());
-  // striping all the directory commands
-  const packageSubDir = lernaConfig.packages.map((packageName: string) =>
+  return JSON.parse(readFileSync(lernaConfigLocation).toString());
+};
+
+/**
+ * Gets the subdirectories of all of the available packages
+ *
+ * @function getSubdirectories
+ * @param {GetConfigOutputInterface} input - lerna.json or whatever config you're using
+ * @return {string[]} - all of the subdirectories available
+ */
+const getSubdirectories = (input: GetConfigOutputInterface): string[] =>
+  input.packages.map((packageName: string) =>
     packageName.replace('/*', '').replace('//', ''),
   );
-  // grabs packages directory array and flattens it
-  const packagesDir = packageSubDir
+
+/**
+ * Gather's the directories of all local packages
+ *
+ * @function getPackageDirectories
+ * @param {string[]} input - subdirectories with their file handling information removed
+ * @return {string[]} - cleaned directories
+ */
+const getPackageDirectories = (input: string[]): string[] =>
+  input
     .map((location: string) =>
       readdirSync(`${location}`).map(
         (packageName: string) => `${location}/${packageName}`,
       ),
     )
     .flat();
-  // Grabs each of the lerna package's package.json and returns it with its location
-  const packageJsonMap = packagesDir.map(
+
+/**
+ * packageJsonMap
+ *
+ * @function getPackageJsonMap
+ * @param {string[]} input - [TODO:description]
+ * @return {[TODO:type]} [TODO:description]
+ */
+const getPackageJsonMap = (input: string[]) =>
+  input.map(
     (packageLocation: string): PackageJsonMapInterface => {
       const stringedJson = readFileSync(
         `${process.cwd()}/${packageLocation}/package.json`,
@@ -59,19 +102,40 @@ async function main(input?: InputInterface): Promise<void> {
       };
     },
   ) as PackageJsonMapInterface[];
-  // gets the package names and lists and their file location
-  const packageNameList = packageJsonMap.map(
-    (packageJson: PackageJsonMapInterface) => ({
-      name: packageJson.content.name,
-      location: packageJson.location,
-    }),
-  );
-  // checks for dependencies that need to be hard copied and returns what and where to copy to
-  const packagesToUpdate = packageJsonMap
+
+/**
+ * Gets the name of each package, and its location
+ *
+ * @function getPackageNameList
+ * @param {PackageJsonMapInterface[]} input
+ * @return {GetPackagesToUpdateInterface}
+ */
+const getPackageNameList = (
+  input: PackageJsonMapInterface[],
+): GetPackagesToUpdateInterface => {
+  const packageNameList = input.map((packageJson: PackageJsonMapInterface) => ({
+    name: packageJson.content.name,
+    location: packageJson.location,
+  }));
+  return {
+    packageNameList,
+    packageJsonMap: input,
+  };
+};
+
+/**
+ * Get a list of packages, and where to copy them to
+ *
+ * @function getPackagesToUpdate
+ * @param {GetPackagesToUpdateInterface} input
+ * @return {CopyToInterface|undefined}
+ */
+const getPackagesToUpdate = (
+  input: GetPackagesToUpdateInterface,
+): (CopyToInterface | undefined)[] =>
+  input.packageJsonMap
     .map((packageJson) => {
-      // checks the deps of each package if any local lerna packages need to be pushed to it.
-      // TODO: convert this to a reduce
-      const deps = packageNameList.map((packageName):
+      const deps = input.packageNameList.map((packageName):
         | CopyToInterface
         | undefined => {
         if (
@@ -93,6 +157,35 @@ async function main(input?: InputInterface): Promise<void> {
     .flat() // flattens the array
     .filter((dep: CopyToInterface | undefined) => dep !== undefined); // removes all undefined
 
+/**
+ * Simple compose
+ *
+ * @function compose
+ * @param {Function[]} fns - an array of functions
+ * @return {[TODO:type]} returns anything
+ */
+const compose = (...fns: Function[]) => (x: any) =>
+  fns.reduceRight((acc, fn) => fn(acc), x);
+
+const composed = compose(
+  getPackagesToUpdate,
+  getPackageNameList,
+  getPackageJsonMap,
+  getPackageDirectories,
+  getSubdirectories,
+  getConfig,
+);
+
+/**
+ * Checks package.json recursively in a lerna project and copies
+ * that file into its node_modules.
+ *
+ * @async
+ * @param {@link InputInterface} input - an input object for settings
+ * @return {Promise<void>} - this is a void
+ **/
+async function main(input?: InputInterface): Promise<void> {
+  const packagesToUpdate = composed(input);
   const fileCopyArray = packagesToUpdate.map(
     async (packageToUpdate: CopyToInterface): Promise<void> => {
       const { copy, to } = packageToUpdate;
